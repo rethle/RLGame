466ac1542dc9b6c078105f8c0d557f3b63032778
dzik-miedzylas <dzikzmiedzylesia@gmail.com>
2024-12-05 12:44:25 +0100

    Created new ColoredString class

---
+++ ColoredString.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RLTools;
+
+public class ColoredString
+{
+    CharInfo[] text_;
+
+    public ColoredString()
+    {
+        text_ = new CharInfo[0];
+    }
+
+    public ColoredString(ColoredString coloredString)
+    {
+        coloredString.text_.CopyTo(text_, 0);
+    }
+    
+    public ColoredString(string text, ConsoleColor foreground, ConsoleColor background)
+    {
+        text_ = new CharInfo[text.Length];
+        for (int i = 0; i < text.Length; i++)
+        {
+            text_[i] = new CharInfo(text[i], foreground, background);
+        }
+    }
+
+    private ColoredString(int length)
+    {
+        text_ = new CharInfo[length];
+    }
+
+    private List<ColoredString> Split(ColoredString colStr, char delimiter)
+    {
+        var colStrings = new List<ColoredString>();
+        ColoredString currentColStr = new ColoredString();
+        for (int i = 0; i < colStr.text_.Length; i++)
+        {
+            if (colStr.text_[i].Character == delimiter && !currentColStr.IsEmpty())
+            {
+                colStrings.Append(currentColStr);
+                currentColStr = new ColoredString();
+                continue;
+            }
+            if(colStr.text_[i].Character == delimiter)
+                continue;
+            currentColStr += colStr.text_[i];
+        }
+
+        return colStrings;
+    }
+
+    public bool IsEmpty()
+    {
+        return text_.Length == 0;
+    }
+    
+    public static ColoredString operator +(ColoredString colStr1, ColoredString colStr2)
+    {
+        ColoredString sum = new ColoredString(colStr1.text_.Length + colStr2.text_.Length);
+        Array.Copy(colStr1.text_, sum.text_, colStr1.text_.Length);
+        Array.Copy(colStr2.text_, 0, sum.text_, colStr1.text_.Length, colStr2.text_.Length);
+        return sum;
+    }
+
+    public static ColoredString operator +(ColoredString colStr, CharInfo colChar)
+    {
+        ColoredString sum = new ColoredString(colStr);
+        sum.text_.Append(colChar);
+        return sum;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,14 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        FConsole.Initialize("Test");
-        Random rand = new Random(1281333);
-        while (true)
-        {
-            FConsole.SetChar((short)rand.Next(0, 256), (short)rand.Next(0, 256),
-                new PixelValue((ConsoleColor)rand.Next(0, 15),
-                    (ConsoleColor)rand.Next(0, 15), (char)rand.Next(50, 100)));
-            FConsole.DrawBuffer();
-        }
+        var a = new ColoredString("Albicla ", ConsoleColor.Red, ConsoleColor.Yellow);
+        var b = new ColoredString("is excellent!", ConsoleColor.Blue, ConsoleColor.Magenta);
+        ColoredString c = a + b;
+        Console.ReadKey();
     }
 }
